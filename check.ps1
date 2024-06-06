8d98af48178060f92de9972ce5e55798a9377005
Tommy Vange Rød <28400191+tommyvange@users.noreply.github.com>
2024-06-06 10:24:43 +0200

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,3 +1,25 @@
+################################################################################
+# Project: tommyvange/printer-driver-management-scripts
+# File: check.ps1
+# Developer: Tommy Vange Rød
+# License: GPL 3.0 License
+#
+# This file is part of "Printer Driver Management Scripts".
+#
+# "Printer Driver Management Scripts" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html#license-text>.
+################################################################################
+
 param (
     [string]$DriverName = "Canon Generic Plus PCL6",
     [bool]$Logging = $false
